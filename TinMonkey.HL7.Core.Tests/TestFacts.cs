e66411f2d4f809e97502066cdb4eafe427b3170d
James Snape <james.snape@hitachivantara.com>
2021-06-05 12:01:19 +0100

    Simpler file header format

--- TinMonkey.HL7.Core.Tests/TestFacts.cs
+++ TinMonkey.HL7.Core.Tests/TestFacts.cs
@@ -1,9 +1,15 @@
-﻿namespace TinMonkey.HL7.Core.Tests
+﻿// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+
+namespace TinMonkey.HL7.Core.Tests
 {
     using Xunit;
 
+    /// <summary>Test facts.</summary>
     public static class TestFacts
     {
+        /// <summary>Testing works.</summary>
         [Fact]
         public static void TestingWorks()
         {
